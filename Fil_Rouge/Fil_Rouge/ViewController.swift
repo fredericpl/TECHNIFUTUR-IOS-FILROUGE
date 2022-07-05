b261a68ad6fa9f776555d7830ce12b7c50ba343f
Fred Doe <frederic.pl@hotmail.com>
2022-07-05 13:47:36 +0200

    first commit

---
+++ Fil_Rouge/Fil_Rouge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fil_Rouge
+//
+//  Created by Student11 on 05/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
